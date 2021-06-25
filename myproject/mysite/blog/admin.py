7cbe0fbaaa9391b158e0dee49561b6dd656dc97e
marlon <751081818@qq.com>
2021-06-25 18:29:58 +0900

    django01

--- myproject/mysite/blog/admin.py
+++ myproject/mysite/blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Entry, Author
+from .models import Entry, Author,Blog
 # Register your models here.
 
-# admin.site.register(Blog)
+admin.site.register(Blog)
 admin.site.register(Author)
 admin.site.register(Entry)
--- myproject/mysite/blog/models.py
+++ myproject/mysite/blog/models.py
@@ -5,8 +5,8 @@ class Blog(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField()
 
-    class Meta:
-        abstract = True
+    # class Meta:
+    #     abstract = True
 
     def __str__(self):
         return self.name
@@ -14,13 +14,55 @@ class Blog(models.Model):
 class Author(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    GRADUATE = 'GR'
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+        (GRADUATE, 'Graduate'),
+    ]
+
+    
+    year_in_school = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FRESHMAN,
+    )
+    MEDIA_CHOICES = [
+    ('Audio', (
+            ('vinyl', 'Vinyl'),
+            ('cd', 'CD'),
+            ('cd', 'CD'),
+            ('cd', 'CD'),
+            ('list','dsad'
+            )
+        )
+    ),
+    ('Video', (
+            ('vhs', 'VHS Tape'),
+            ('dvd', 'DVD'),
+            ('cd', 'CD'),
+            ('cd', 'CD'),
+            ('cd', 'CD'),
+
+        )
+    ),
+    ('unknown', 'Unknown'),
+    ]
+
+    media_type = models.CharField(max_length=100,choices=MEDIA_CHOICES,default="unknown")
 
     def __str__(self):
         return self.name
 
 
 class Entry(models.Model):
-    # blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     headline = models.CharField(max_length=255)
     body_text = models.TextField()
     pub_date = models.DateField()
